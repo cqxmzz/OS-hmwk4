b2b81cb427c499c508819ceac7e2c746a71d445c
W4118 Student <w4118@w4118>
2014-10-23 01:49:30 -0400

    All cpus are up

--- flo-kernel/arch/arm/mach-msm/msm_mpdecision.c
+++ flo-kernel/arch/arm/mach-msm/msm_mpdecision.c
@@ -225,7 +225,7 @@ static void bring_down_cpu(int cpu)
 
 	BUG_ON(cpu == 0);
 	cpu_action_time_ms = ktime_to_ms(ktime_get());
-	ret = cpu_down(cpu);
+	ret = 0;//cpu_down(cpu);
 	if (ret) {
 		pr_debug("Error %d offline" "core %d\n", ret, cpu);
 	} else {
