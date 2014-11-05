5ce4d9725aa5c5ea402ff6449a32e7cadca6a639
Wendan Kang <Wendan@dyn-160-39-216-60.dyn.columbia.edu>
2014-11-05 11:28:46 -0500

    extern function in sched.h

--- flo-kernel/kernel/sched/sched.h
+++ flo-kernel/kernel/sched/sched.h
@@ -204,6 +204,8 @@ extern void init_tg_rt_entry(struct task_group *tg, struct rt_rq *rt_rq,
 		struct sched_rt_entity *parent);
 
 /* Wendan Kang: may need extern some load balance function here*/
+extern void free_grr_sched_group(struct task_group *tg);
+extern int alloc_grr_sched_group(struct task_group *tg, struct task_group *parent);
 extern void init_tg_grr_entry(struct task_group *tg, struct grr_rq *grr_rq,
 		struct sched_grr_entity *grr_se, int cpu,
 		struct sched_grr_entity *parent);
