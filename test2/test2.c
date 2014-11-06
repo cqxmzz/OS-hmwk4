a949fe7cddff23acd19edcd96b94b8e4811d3ceb
Qiming Chen <qc2195@columbia.edu>
2014-11-06 01:03:37 -0500

    finally kind of works

--- test2/test2.c
+++ test2/test2.c
@@ -1,17 +1,23 @@
+#include <errno.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sched.h>
+#include "../flo-kernel/include/linux/sched.h"
 
 int main(int argc, char **argv) 
 {
 
-	struct sched_param param;
-	param.sched_priority = 99;
+	struct sched_param p;
+	//p.sched_priority = 99;
 	int i = 0;
-	scanf("%i", &i);
-	sched_setscheduler(i, 6, & param);
-	printf("OK");
+	scanf("%d", &i);
+	printf("%d", i);
+	pid_t t = (pid_t) i;
+	printf("%d",sched_setscheduler(t, 6, &p));
+		printf("OK");
+	printf(strerror(errno));
 	return 0;
 }
